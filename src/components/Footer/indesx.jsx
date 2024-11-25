6218323fab77c9c1608df63c3dffe007fce6eeb0
Raju Mannem <rajumannem71@gmail.com>
2024-11-25 18:27:26 +0530

    footer added

---
+++ src/components/Footer/indesx.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center h-full bg-gray-300 py-4">
+      <p>
+        developed by{" "}
+        <a
+          href="https://github.com/raju-mannem"
+          target="_blank"
+          rel="noreferrer"
+        >
+          raju mannem
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
