4d4b96c9b09b773dee41d7eadb4082ade1a5080a
Raju Mannem <rajumannem71@gmail.com>
2024-11-25 18:27:44 +0530

    dashboard add v1

---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="h-screen">
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="w-full max-w-2xl">
+          <h1 className="text-3xl font-bold text-center">Dasbhoard </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
